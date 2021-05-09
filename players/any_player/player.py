4fc76233a44e7f95d3c2055a705f180a67d2028d
Olivier Juan <olivier.juan@gmail.com>
2021-05-09 22:37:26 +0200

    prepare Player

--- players/any_player/player.py
+++ players/any_player/player.py
@@ -1,12 +1,26 @@
 # python 3
 # this class combines all basic features of a generic player
+import numpy as np
 
 
 class Player:
 
-	def __init__(self, parameters):
+	def __init__(self):
 		# some player might not have parameters
-		self.parameters = parameters
+		self.parameters = 0
+		self.horizon = 48
+
+	def set_scenario(self, scenario_data):
+		self.data = scenario_data
+
+	def set_prices(self, prices):
+		self.prices = prices
+
+	def compute_all_load(self):
+		load = np.zeros(self.horizon)
+		# for time in range(self.horizon):
+		# 	load[time] = self.compute_load(time)
+		return load
 
 	def take_decision(self, time):
 		# TO BE COMPLETED
@@ -16,10 +30,6 @@ class Player:
 		load = self.take_decision(time)
 		# do stuff ?
 		return load
-
-	def observe(self, time, data, price):
-		# save observations for decision making
-		pass
 
 	def reset(self):
 		# reset all observed data
